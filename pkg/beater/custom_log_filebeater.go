a1b9f25d583bbfe096ed5aa0743b515a773994dc
Sagar <skalburgi@axway.com>
2021-02-01 17:17:39 -0700

    Add logic to parse request logs and build transaction summary event

--- pkg/beater/custom_log_filebeater.go
+++ pkg/beater/custom_log_filebeater.go
@@ -1,64 +1,46 @@
 package beater
 
 import (
-	"github.com/Axway/agent-sdk/pkg/traceability"
-	agentErrors "github.com/Axway/agent-sdk/pkg/util/errors"
-	hc "github.com/Axway/agent-sdk/pkg/util/healthcheck"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/logp"
 
-	config "github.com/Axway/agents-kong/pkg/config/traceability"
 	"github.com/Axway/agents-kong/pkg/gateway"
 )
 
 // customLogBeater configuration.
 type customLogBeater struct {
 	done           chan struct{}
-	logReader      *gateway.LogReader
 	eventProcessor *gateway.EventProcessor
 	client         beat.Client
 	eventChannel   chan string
 }
 
-var bt *customLogBeater
-var gatewayConfig *config.GatewayConfig
-
-// New creates an instance of aws_apigw_traceability_agent.
-func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
+// New creates an instance of kong_traceability_agent.
+func New(*beat.Beat, *common.Config) (beat.Beater, error) {
 	bt := &customLogBeater{
 		done:         make(chan struct{}),
 		eventChannel: make(chan string),
 	}
 
-	var err error
-	bt.logReader, err = gateway.NewLogReader(gatewayConfig, bt.eventChannel)
-	bt.eventProcessor = gateway.NewEventProcessor(gatewayConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	if !gatewayConfig.ProcessOnInput {
-		traceability.SetOutputEventProcessor(bt.eventProcessor)
-	}
+	bt.eventProcessor = gateway.NewEventProcessor()
 
 	// Validate that all necessary services are up and running. If not, return error
-	if hc.RunChecks() != hc.OK {
-		return nil, agentErrors.ErrInitServicesNotReady
-	}
+	//if hc.RunChecks() != hc.OK {
+	//	return nil, agentErrors.ErrInitServicesNotReady
+	//}
 
 	return bt, nil
 }
 
-// SetGatewayConfig - set parsed gateway config
-func SetGatewayConfig(gatewayCfg *config.GatewayConfig) {
-	gatewayConfig = gatewayCfg
-}
-
-// Run starts awsApigwTraceabilityAgent.
+// Run starts kong_traceability_agent.
 func (bt *customLogBeater) Run(b *beat.Beat) error {
-	logp.Info("apic_traceability_agent is running! Hit CTRL-C to stop it.")
+	logp.Info("kong_traceability_agent is running! Hit CTRL-C to stop it.")
 
 	var err error
 	bt.client, err = b.Publisher.Connect()
@@ -66,32 +48,49 @@ func (bt *customLogBeater) Run(b *beat.Beat) error {
 		return err
 	}
 
-	bt.logReader.Start()
+	http.HandleFunc("/requestlogs", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+
+		if err != nil {
+			fmt.Errorf("Error while reading request body: %s", err)
+		}
+
+		w.WriteHeader(200)
+		bt.processAndDispatchEvent(string(body))
+	})
+
+	/* Start a new HTTP server in a separate Go routine that will be the target
+	   for the HTTP Log plugin. It should write events it gets to eventChannel */
+	go func() {
+		if err := http.ListenAndServe(":9000", nil); err != nil {
+			log.Fatalf("Unable to start the HTTP Server: %s", err)
+		}
+		log.Print("Started HTTP server on port 9000 to receive request logs")
+	}()
 
 	for {
 		select {
 		case <-bt.done:
 			return nil
-		case eventData := <-bt.eventChannel:
-			if gatewayConfig.ProcessOnInput {
-				eventsToPublish := bt.eventProcessor.ProcessRaw([]byte(eventData))
-				if eventsToPublish != nil {
-					bt.client.PublishAll(eventsToPublish)
-				}
-			} else {
-				eventToPublish := beat.Event{
-					Fields: common.MapStr{
-						"message": eventData,
-					},
-				}
-				bt.client.Publish(eventToPublish)
-			}
 		}
 	}
 }
 
-// Stop stops customLogTraceabilityAgent.
+// Stop stops kong_traceability_agent.
 func (bt *customLogBeater) Stop() {
 	bt.client.Close()
 	close(bt.done)
+}
+
+func (bt *customLogBeater) processAndDispatchEvent(logEvent string) {
+	eventsToPublish := bt.eventProcessor.ProcessRaw([]byte(logEvent))
+	if eventsToPublish != nil {
+		bt.client.PublishAll(eventsToPublish)
+	}
 }
--- pkg/gateway/definitions.go
+++ pkg/gateway/definitions.go
@@ -1,28 +1,77 @@
 package gateway
 
-// Headers - Type for request/response headers
-type Headers map[string]string
-
-// GwTransaction - Type for gateway transaction detail
-type GwTransaction struct {
-	ID              string  `json:"id"`
-	SourceHost      string  `json:"srcHost"`
-	SourcePort      int     `json:"srcPort"`
-	DesHost         string  `json:"destHost"`
-	DestPort        int     `json:"destPort"`
-	URI             string  `json:"uri"`
-	Method          string  `json:"method"`
-	StatusCode      int     `json:"statusCode"`
-	RequestHeaders  Headers `json:"requestHeaders"`
-	ResponseHeaders Headers `json:"responseHeaders"`
-	RequestBytes    int     `json:"requestByte"`
-	ResponseBytes   int     `json:"responseByte"`
+// KongTrafficLogEntry - Represents the structure of log entry the agent will receive from Kong's HTTP Log Plugin
+type KongTrafficLogEntry struct {
+	ClientIP    string     `json:"client_ip"`
+	StartedAt   int64      `json:"started_at"`
+	UpstreamURI string     `json:"upstream_uri"`
+	Latencies   *Latencies `json:"latencies"`
+	Request     *Request   `json:"request"`
+	Response    *Response  `json:"response"`
+	Route       *Route     `json:"route"`
+	Service     *Service   `json:"service"`
+	Tries       []*Tries   `json:"tries"`
 }
 
-// GwTrafficLogEntry - Represents the structure of log entry the agent will receive
-type GwTrafficLogEntry struct {
-	TraceID             string        `json:"traceId"`
-	APIName             string        `json:"apiName"`
-	InboundTransaction  GwTransaction `json:"inbound"`
-	OutboundTransaction GwTransaction `json:"outbound"`
+type Latencies struct {
+	Request int `json:"request"`
+	Kong    int `json:"kong"`
+	Proxy   int `json:"proxy"`
+}
+
+type Request struct {
+	QueryString map[string]string `json:"querystring"`
+	Size        int               `json:"size"`
+	URI         string            `json:"uri"`
+	URL         string            `json:"url"`
+	Headers     map[string]string `json:"headers"`
+	Method      string            `json:"method"`
+}
+
+type Response struct {
+	Headers map[string]string `json:"headers"`
+	Status  int               `json:"status"`
+	Size    int               `json:"size"`
+}
+
+type Route struct {
+	ID                      string            `json:"id"`
+	UpdatedAt               int               `json:"updated_at"`
+	Protocols               []string          `json:"protocols"`
+	StripPath               bool              `json:"strip_path"`
+	CreatedAt               int               `json:"created_at"`
+	WsID                    string            `json:"ws_id"`
+	Service                 map[string]string `json:"service"`
+	Name                    string            `json:"name"`
+	Hosts                   []string          `json:"hosts"`
+	PreserveHost            bool              `json:"preserve_host"`
+	RegexPriority           int               `json:"regex_priority"`
+	Paths                   []string          `json:"paths"`
+	ResponseBuffering       bool              `json:"response_buffering"`
+	HttpsRedirectStatusCode int               `json:"https_redirect_status_code"`
+	PathHandling            string            `json:"path_handling"`
+	RequestBuffering        bool              `json:"request_buffering"`
+}
+
+type Service struct {
+	Host           string `json:"host"`
+	CreatedAt      int    `json:"created_at"`
+	ConnectTimeout int    `json:"connect_timeout"`
+	ID             string `json:"id"`
+	Protocol       string `json:"protocol"`
+	Name           string `json:"name"`
+	ReadTimeout    int    `json:"read_timeout"`
+	Port           int    `json:"port"`
+	Path           string `json:"path"`
+	UpdatedAt      int    `json:"updated_at"`
+	WriteTimeout   int    `json:"write_timeout"`
+	Retries        int    `json:"retries"`
+	WsID           string `json:"ws_id"`
+}
+
+type Tries struct {
+	BalancerLatency int    `json:"balancer_latency"`
+	Port            int    `json:"port"`
+	BalancerStart   int64  `json:"balancer_start"`
+	IP              string `json:"ip"`
 }
