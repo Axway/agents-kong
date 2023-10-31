9a31161d83fd0a466026400b703758fa9235ac7b
Jason Collins <jcollins@axway.com>
2023-10-31 14:38:57 -0700

    use klib string

--- pkg/kong/provisioning.go
+++ pkg/kong/provisioning.go
@@ -9,7 +9,7 @@ import (
 func (k KongClient) CreateConsumer(ctx context.Context, id, name string) (*klib.Consumer, error) {
 	// validate that the consumer does not already exist
 	log := k.logger.WithField("consumerID", id).WithField("consumerName", name)
-	consumer, err := k.Consumers.Get(ctx, &id)
+	consumer, err := k.Consumers.Get(ctx, klib.String(id))
 	if err == nil {
 		log.Debug("found existing consumer")
 		return consumer, err
@@ -17,20 +17,20 @@ func (k KongClient) CreateConsumer(ctx context.Context, id, name string) (*klib.
 
 	log.Debug("creating new consumer")
 	return k.Consumers.Create(ctx, &klib.Consumer{
-		CustomID: &id,
-		Username: &name,
+		CustomID: klib.String(id),
+		Username: klib.String(name),
 	})
 }
 
 func (k KongClient) DeleteConsumer(ctx context.Context, id string) error {
 	// validate that the consumer has not already been removed
 	log := k.logger.WithField("consumerID", id)
-	_, err := k.Consumers.Get(ctx, &id)
+	_, err := k.Consumers.Get(ctx, klib.String(id))
 	if err != nil {
 		log.Debug("could not get consumer")
 		return nil
 	}
 
 	log.Debug("deleting consumer")
-	return k.Consumers.Delete(ctx, &id)
+	return k.Consumers.Delete(ctx, klib.String(id))
 }
