fdf8a92d7decbb5c54e64bcfc5358c70f96d472e
Alin Rosca <acrosca@axway.com>
2023-11-09 11:18:24 +0200

    APIGOV-26574 access request

---
+++ pkg/subscription/access/access.go
@@ -0,0 +1,117 @@
+package access
+
+import (
+	"context"
+
+	"github.com/Axway/agent-sdk/pkg/apic/provisioning"
+	sdkUtil "github.com/Axway/agent-sdk/pkg/util"
+	"github.com/Axway/agent-sdk/pkg/util/log"
+	"github.com/Axway/agents-kong/pkg/common"
+)
+
+const (
+	logFieldAppID     = "appID"
+	logFieldAppName   = "appName"
+	logFieldServiceID = "serviceID"
+	logFieldRouteID   = "routeID"
+)
+
+type accessClient interface {
+	AddManagedAppACL(ctx context.Context, managedAppID, routeID string) error
+	RemoveManagedAppACL(ctx context.Context, serviceID, routeID, managedAppID string) error
+	AddQuota(ctx context.Context, serviceID, managedAppID string, quota provisioning.Quota) error
+}
+
+type accessRequest interface {
+	GetApplicationDetailsValue(key string) string
+	GetInstanceDetails() map[string]interface{}
+	GetQuota() provisioning.Quota
+}
+
+type AccessProvisioner struct {
+	ctx       context.Context
+	logger    log.FieldLogger
+	client    accessClient
+	quota     provisioning.Quota
+	routeID   string
+	serviceID string
+	appID     string
+}
+
+func NewAccessProvisioner(ctx context.Context, client accessClient, request accessRequest) AccessProvisioner {
+	instDetails := request.GetInstanceDetails()
+	serviceID := sdkUtil.ToString(instDetails[common.AttrServiceId])
+	routeID := sdkUtil.ToString(instDetails[common.AttrRouteId])
+
+	a := AccessProvisioner{
+		ctx: context.Background(),
+		logger: log.NewFieldLogger().
+			WithComponent("AccessProvisioner").
+			WithPackage("access"),
+		client:    client,
+		quota:     request.GetQuota(),
+		routeID:   routeID,
+		serviceID: serviceID,
+		appID:     request.GetApplicationDetailsValue(common.AttrAppID),
+	}
+
+	if a.serviceID != "" {
+		a.logger = a.logger.WithField(logFieldServiceID, a.serviceID)
+	}
+	if a.routeID != "" {
+		a.logger = a.logger.WithField(logFieldRouteID, a.routeID)
+	}
+	if a.appID != "" {
+		a.logger = a.logger.WithField(logFieldAppID, a.appID)
+	}
+	return a
+}
+
+func (a AccessProvisioner) Provision() (provisioning.RequestStatus, provisioning.AccessData) {
+	a.logger.Info("provisioning access")
+
+	rs := provisioning.NewRequestStatusBuilder()
+	if a.appID == "" {
+		a.logger.Error("could not find the managed application ID on the resource")
+		return rs.SetMessage("managed application ID not found").Failed(), nil
+	}
+
+	if a.quota.GetInterval().String() == provisioning.Weekly.String() {
+		a.logger.Debug("weekly quota interval is not supported")
+		return rs.SetMessage("weekly quota is not supported by kong").Failed(), nil
+	}
+
+	err := a.client.AddManagedAppACL(a.ctx, a.appID, a.routeID)
+	if err != nil {
+		a.logger.WithError(err).Error("failed to provide access to managed application")
+		return rs.SetMessage("could not provide access to consumer in kong").Failed(), nil
+	}
+
+	err = a.client.AddQuota(a.ctx, a.serviceID, a.appID, a.quota)
+	if err != nil {
+		a.logger.WithError(err).Error("failed to create quota for consumer")
+		return rs.SetMessage("could not create limits for consumer in kong").Failed(), nil
+	}
+
+	a.logger.Info("provisioned access")
+	return rs.Success(), nil
+}
+
+func (a AccessProvisioner) Deprovision() provisioning.RequestStatus {
+	a.logger.Info("deprovisioning access")
+
+	rs := provisioning.NewRequestStatusBuilder()
+	if a.appID == "" {
+		a.logger.Error("could not find the managed application ID on the resource")
+		return rs.SetMessage("managed application ID not found").Failed()
+	}
+
+	err := a.client.RemoveManagedAppACL(a.ctx, a.serviceID, a.routeID, a.appID)
+	if err != nil {
+		a.logger.WithError(err).Error("failed to remove managed app from ACL")
+		return rs.SetMessage("could not remove consumer from ACL").Failed()
+	}
+
+	a.logger.Info("deprovisioned access")
+	return rs.Success()
+}
--- pkg/subscription/provision.go
+++ pkg/subscription/provision.go
@@ -10,11 +10,11 @@ import (
 
 	"github.com/Axway/agent-sdk/pkg/agent"
 	"github.com/Axway/agent-sdk/pkg/apic/provisioning"
-	sdkUtil "github.com/Axway/agent-sdk/pkg/util"
 	"github.com/Axway/agent-sdk/pkg/util/log"
 
 	"github.com/Axway/agents-kong/pkg/common"
 	"github.com/Axway/agents-kong/pkg/kong"
+	"github.com/Axway/agents-kong/pkg/subscription/access"
 	"github.com/Axway/agents-kong/pkg/subscription/application"
 )
 
@@ -73,99 +73,6 @@ func (p provisioner) CredentialUpdate(request provisioning.CredentialRequest) (p
 	return Failed(provisioning.NewRequestStatusBuilder(), errors.New(errorMsg)), nil
 }
 
-func (p provisioner) AccessRequestProvision(request provisioning.AccessRequest) (provisioning.RequestStatus, provisioning.AccessData) {
-	p.logger.Info("provisioning access request")
-	agentTag := "amplify-agent"
-	ctx := context.Background()
-	rs := provisioning.NewRequestStatusBuilder()
-	instDetails := request.GetInstanceDetails()
-	serviceId := sdkUtil.ToString(instDetails[common.AttrServiceId])
-	routeId := sdkUtil.ToString(instDetails[common.AttrRouteId])
-	if serviceId == "" {
-		return Failed(rs, notFound(common.AttrServiceId)), nil
-	}
-	if routeId == "" {
-		return Failed(rs, notFound(common.AttrRouteId)), nil
-	}
-	kongApplicationId := request.GetApplicationDetailsValue(common.AttrAppID)
-	if kongApplicationId == "" {
-		return Failed(rs, fmt.Errorf("kong application id not set")), nil
-	}
-	plugins := kong.Plugins{PluginLister: p.kc.Plugins}
-	ep, err := plugins.GetEffectivePlugins(routeId, serviceId)
-	if err != nil {
-		return Failed(rs, fmt.Errorf("failed to list route plugins: %w", err)), nil
-	}
-	plugin, ok := ep["acl"]
-	if !ok {
-		log.Infof("ACL Plugin is not configured on route / service %s", serviceId)
-		_, err := p.kc.Plugins.CreateForService(ctx, &serviceId, plugin)
-		if err != nil {
-			return Failed(rs, fmt.Errorf("failed to add ACL pluing to service: %w", err)), nil
-		}
-	}
-	group := common.AclGroup
-	consumerTags := []*string{&agentTag}
-	_, err = p.kc.ACLs.Create(ctx, &kongApplicationId, &klib.ACLGroup{Group: &group, Tags: consumerTags})
-	if err != nil {
-		return Failed(rs, fmt.Errorf("failed to add acl group on consumer: %w", err)), nil
-	}
-	// process access request create
-	rs.AddProperty(common.AttrAppID, kongApplicationId)
-	amplifyQuota := request.GetQuota()
-	if amplifyQuota != nil {
-		planName := amplifyQuota.GetPlanName()
-		planDesc := amplifyQuota.GetPlanName()
-		quotaLimit := int(amplifyQuota.GetLimit())
-		p.logger.Info(" Plan name :%s, Plan Description :%s Quota Limit: %s", planName, planDesc, quotaLimit)
-		config := klib.Configuration{
-			"limit":  []interface{}{quotaLimit},
-			"policy": "local",
-		}
-		p.logger.Info("%v", config)
-		//err := addRateLimit(p.kc, ctx, config, "")
-		//if err != nil {
-		//	return nil, nil
-		//}
-
-		//amplifyQuota.GetInterval().
-	}
-	p.logger.
-		WithField("api", serviceId).
-		WithField("app", request.GetApplicationName()).
-		Info("granted access")
-	return rs.Success(), nil
-}
-func (p provisioner) AccessRequestDeprovision(request provisioning.AccessRequest) provisioning.RequestStatus {
-	p.logger.Info("deprovisioning access request")
-	ctx := context.Background()
-	rs := provisioning.NewRequestStatusBuilder()
-	instDetails := request.GetInstanceDetails()
-	serviceId := sdkUtil.ToString(instDetails[common.AttrServiceId])
-	routeId := sdkUtil.ToString(instDetails[common.AttrRouteId])
-	if serviceId == "" {
-		return Failed(rs, notFound(common.AttrServiceId))
-	}
-	if routeId == "" {
-		return Failed(rs, notFound(common.AttrRouteId))
-	}
-	// process access request delete
-	kongConsumerId := request.GetAccessRequestDetailsValue(common.AttrAppID)
-	//GetApplicationDetailsValue(common.AttrAppID)
-	if kongConsumerId == "" {
-		return Failed(rs, notFound(common.AttrAppID))
-	}
-	group := common.AclGroup
-	err := p.kc.ACLs.Delete(ctx, &kongConsumerId, &group)
-	if err != nil {
-		return Failed(rs, fmt.Errorf("failed to remove acl group on consumer: %w", err))
-	}
-	p.logger.
-		WithField("api", serviceId).
-		WithField("app", request.GetApplicationName()).
-		Info("removed access")
-	return rs.Success()
-}
 func (p provisioner) CredentialProvision(request provisioning.CredentialRequest) (provisioning.RequestStatus, provisioning.Credential) {
 
 	p.logger.Info("provisioning credentials")
@@ -218,19 +125,19 @@ func GetProvisionKeyPropertyBuilder() provisioning.PropertyBuilder {
 		IsString()
 }
 
-//func addRateLimit(kc *klib.Client, ctx context.Context, config map[string]interface{}, serviceId string) error {
-//	pluginName := "rate-limiting"
-//	rateLimitPlugin := klib.Plugin{
-//		Name:   &pluginName,
-//		Config: config,
-//	}
-//	kc.Do(ctx)
-//	//_, err := kc.Consumers.C(ctx, &serviceId, &rateLimitPlugin)
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
+func addRateLimit(kc *klib.Client, ctx context.Context, config map[string]interface{}, serviceId string) error {
+	pluginName := "rate-limiting"
+	rateLimitPlugin := klib.Plugin{
+		Name:     &pluginName,
+		Config:   config,
+		Consumer: &klib.Consumer{},
+	}
+	_, err := kc.Plugins.CreateForService(ctx, &serviceId, &rateLimitPlugin)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func (p provisioner) ApplicationRequestProvision(request provisioning.ApplicationRequest) provisioning.RequestStatus {
 	return application.NewApplicationProvisioner(context.Background(), p.client, request).Provision()
@@ -238,4 +145,12 @@ func (p provisioner) ApplicationRequestProvision(request provisioning.Applicatio
 
 func (p provisioner) ApplicationRequestDeprovision(request provisioning.ApplicationRequest) provisioning.RequestStatus {
 	return application.NewApplicationProvisioner(context.Background(), p.client, request).Deprovision()
+}
+
+func (p provisioner) AccessRequestProvision(request provisioning.AccessRequest) (provisioning.RequestStatus, provisioning.AccessData) {
+	return access.NewAccessProvisioner(context.Background(), p.client, request).Provision()
+}
+
+func (p provisioner) AccessRequestDeprovision(request provisioning.AccessRequest) provisioning.RequestStatus {
+	return access.NewAccessProvisioner(context.Background(), p.client, request).Deprovision()
 }
