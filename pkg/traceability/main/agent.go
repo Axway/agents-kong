2ad84439539d746600a35071561f0d2d6b22cc40
Jason Collins <jcollins@axway.com>
2023-11-28 13:54:56 -0700

    updates for helm charts

--- pkg/traceability/main/agent.go
+++ pkg/traceability/main/agent.go
@@ -11,6 +11,13 @@ import (
 
 func main() {
 	os.Setenv("AGENTFEATURES_VERSIONCHECKER", "false")
+
+	// use the pod name as the agent name
+	pod_name := os.Getenv("POD_NAME")
+	if pod_name != "" {
+		os.Setenv("CENTRAL_AGENTNAME", pod_name)
+	}
+
 	if err := traceability.TraceCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
