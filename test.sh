e938c51cd502aa9f333f18f2cab76efbbea5889d
dfeldick <dfeldick@axway.com>
2025-04-08 09:49:16 -0700

    APIGOV-30178 - test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-echo "url1 $TEAMS_WEBHOOK_URL"
-echo "url2 ${{ env.TEAMS_WEBHOOK_URL }}"
-echo "url3 ${{ vars.TEAMS_WEBHOOK_URL }}"
+echo "url1 ${TEAMS_WEBHOOK_URL}"
+echo "url2 ${env.TEAMS_WEBHOOK_URL}"
+echo "url3 ${vars.TEAMS_WEBHOOK_URL}"
